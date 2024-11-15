9039bae97bde4a51155d13492c1467ce452ce1b2
Darius <dwils22@uic.edu>
2024-11-14 23:18:47 -0600

    cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
 };
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use((req, res, next) => {
